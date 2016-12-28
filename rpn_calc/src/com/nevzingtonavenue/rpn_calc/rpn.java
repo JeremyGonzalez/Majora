9ff387be9910cd6d3945d3e2778bc6dd303a0784
taylorswift <jeremy.gonzalez.alfonso@gmail.com>
2016-12-28 04:47:41 -0500

    setting baseline for rpn class

--- rpn_calc/src/com/nevzingtonavenue/rpn_calc/rpn.java
+++ rpn_calc/src/com/nevzingtonavenue/rpn_calc/rpn.java
@@ -1,7 +1,24 @@
 package com.nevzingtonavenue.rpn_calc;
 
 public class rpn {
-	public static void main(String[] args){
-		System.out.println("No Errors at this time...");
+	
+	private int[] stk; // stack used to hold operands.
+	
+	public void push(int val){} // pushes an int onto the stack.
+	
+	public int pop(){return 0;} // pops off an int for various uses.
+	
+	public void operation(String a){
+		// Determine what op will be performed via the string input.
+		// Pop off the previous 2 int and perform specified op.
+		// Push result back on stk.
 	}
+	
+	// operation() shall invoke appropriate method listed below.
+	// These methods shall merely perform the maths op required.
+	private void add(){}
+	private void minus(){}
+	private void multi(){}
+	private void divide(){}
+
 }
